02ca06d194512422cb6d5e7e2f3688ecf7902c9e
TeePC <andretthom@gmail.com>
2023-05-15 20:11:25 -0400

    Added slider, Improved css on option buttons

--- OneDrive/Desktop/Code Files/Etch/script.js
+++ OneDrive/Desktop/Code Files/Etch/script.js
@@ -1,6 +1,24 @@
-const container = document.querySelector('.grid-container');
-for (let i = 0; i < 16*16; i++) {
+var slider = document.getElementById("myRange");
+
+
+
+
+const container = document.querySelector('.grid__container');
+for (let i = 0; i < 5*5; i++) {
   const square = document.createElement('div');
-  square.classList.add('grid-item');
+  square.classList.add('grid__item');
   container.appendChild(square);
 }
+
+
+function create_grid(num) {
+    for (let i = 0; i < num*num; i++) {
+        const square = document.createElement('div');
+        square.classList.add('grid__item');
+        container.appendChild(square);
+      }
+
+}
+
+
+
